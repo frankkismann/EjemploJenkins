663d21074b08266fb6ba45f5a9790a9d9d17ce81
Frank <kismannf@gmail.com>
2023-12-11 16:34:04 -0300

    fix

--- src/main/java/com/mycompany/ejemplojenkins/EjemploJenkins.java
+++ src/main/java/com/mycompany/ejemplojenkins/EjemploJenkins.java
@@ -2,6 +2,6 @@ package com.mycompany.ejemplojenkins;
 public class EjemploJenkins {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundo!");
     }
 }
